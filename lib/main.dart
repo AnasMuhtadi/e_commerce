717e1e1f18f5597b6776e46a3c9e6c1962bc2bd0
AnasMuhtadi <amuhtadi966@gmail.com>
2023-03-10 21:34:00 +0200

    git commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,6 @@ import 'package:untitled/routing/navigation.dart';
 import 'package:untitled/routing/router.dart';
 import 'package:untitled/routing/routes.dart';
 import 'package:untitled/ui/pages/home.dart';
-import 'package:untitled/ui/pages/homePages/check_out.dart';
-import 'package:untitled/ui/pages/homePages/my_cart.dart';
-import 'package:untitled/ui/pages/homePages/my_order.dart';
-import 'package:untitled/ui/pages/homePages/new_arrival.dart';
-import 'package:untitled/ui/pages/homePages/payment_congratulation.dart';
-import 'package:untitled/ui/pages/homePages/product_details.dart';
-import 'package:untitled/ui/pages/homePages/profile.dart';
-import 'package:untitled/ui/pages/signUp.dart';
-import 'package:untitled/ui/pages/splach.dart';
 import 'package:untitled/utils/app_constant.dart';
 
 
